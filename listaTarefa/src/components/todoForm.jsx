309d06b73fe55341a4095e392e4bc1d15f612908
Matheus Damasceno <matheusfjxj@gmail.com>
2025-01-09 22:09:13 -0300

    feat: State

--- listaTarefa/src/components/todoForm.jsx
+++ listaTarefa/src/components/todoForm.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import {useState} from "react";
 
 const todoForm = () => {
+    const [value, setValue] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!value || !categoria) {return}
+        
+        setValue("")
+        setCategoria("") 
+    }
+
     return (
         <div className="todo-form">
             <h2>Cria nova tarefa</h2>
-            <form>
-                <input type="text" placeholder="Digite o título"/>
-                <select>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={value} placeholder="Digite o título" onChange={(e) => setValue(e.target.value)}/>
+                <select value={categoria} onChange={(e) => setCategoria(e.target.value)}>
                     <option value="">Selecione uma opção</option>
                     <option value="Trabalho">Trabalho</option>
                     <option value="Pessoal">Pessoal</option>
